4d379ad2588a3bcdf83da24f94e727ecdf4b3595
lovekushgupta1999 <guptalovekush64@gmail.com>
2025-02-16 18:01:10 +0530

    Nav and Hero section

---
+++ portfolio/src/Navbar.jsx
@@ -0,0 +1,16 @@
+const Nav=()=>{
+    return(
+        <div className="navbar">
+        <div><h1>logo</h1></div>
+        <div>
+            <div><li>Home</li></div>
+            <div><li>About</li></div>
+            <div><li>Skills</li></div>
+            <div><li>Services</li></div>
+            <div><li>Portfolio</li></div>
+            <div><li>Contact</li></div>
+        </div>
+        <div><button>Download CV</button></div>
+        </div>
+    )
+}
